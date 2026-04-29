1ca9b5a47bd213e67abf4218aa77897ecad7084d
jobe2208 <jobe2208@student.miun.se>
2026-04-29 13:55:06 +0200

    Initial commit

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-# An Image analysis project by Johannes and Erik
+import torch
+import torch.nn as nn
+from torch.optim import AdamW
+from torch.optim.lr_scheduler import CosineAnnealingLR
+from torch.utils.tensorboard import SummaryWriter
+from pathlib import Path
+import time
+
+# some libraries AI recommend me to download.
+
